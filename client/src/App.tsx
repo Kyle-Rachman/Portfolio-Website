6f6a71bb0885cef22656edc3921a3d0601addce8
Kyle-Rachman <kcrachman@gmail.com>
2023-09-25 15:23:48 -0700

    Removed BE for now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './views/Home/Home'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar></Navbar>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<p>Working on it</p>}></Route>
+          <Route path='/' element={<Home></Home>}></Route>
         </Routes>
       </BrowserRouter>
     </>

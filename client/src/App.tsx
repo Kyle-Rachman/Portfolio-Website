60117b41b57fe576dd6f34038d8b0ccd6a9c58ea
Kyle-Rachman <kcrachman@gmail.com>
2023-09-25 14:52:29 -0700

    Getting started

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<p>Working on it</p>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App

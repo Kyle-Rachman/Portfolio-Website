db80dc5f14c69c8dcc356bd60cf0014828746210
Kyle-Rachman <kcrachman@gmail.com>
2023-09-26 16:35:33 -0700

    Main section of home page done

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './Home.module.css'
 
 const Home = () => {
+
     return (
         <>
             <div id={styles.container}>
@@ -12,15 +13,23 @@ const Home = () => {
                         <h2 className={`text-3xl text-gray-300 ${styles.subhero}`}>Web Design and Development</h2>
                     </div>
                     <div className={styles.skills}>
-                        <img src="/placeholder-logo-1.png" alt="" className={styles.skill}/>
-                        <img src="/placeholder-logo-1.png" alt="" className={styles.skill}/>
-                        <img src="/placeholder-logo-1.png" alt="" className={styles.skill}/>
-                        <img src="/placeholder-logo-1.png" alt="" className={styles.skill}/>
-                        <img src="/placeholder-logo-1.png" alt="" className={styles.skill}/>
-                        <img src="/placeholder-logo-1.png" alt="" className={styles.skill}/>
-                        <img src="/placeholder-logo-1.png" alt="" className={styles.skill}/>
-                        <img src="/placeholder-logo-1.png" alt="" className={styles.skill}/>
-                        <img src="/placeholder-logo-1.png" alt="" className={styles.skill}/>
+                        <img src="/logos/HTML5_Badge.svg" alt="HTML logo" className={styles.skill}/>
+                        <img src="/logos/CSS3_logo.svg" alt="CSS logo" className={styles.skill}/>
+                        <img src="/logos/logo-javascript.svg" alt="Javascript logo" className={styles.skill}/>
+                        <img src="/logos/Typescript_logo_2020.svg" alt="Typescript logo" className={styles.skill}/>
+                        <img src="/logos/logo-python.svg" alt="Python logo" className={styles.skill}/>
+                        <img src="/logos/Bootstrap_logo.svg" alt="Bootstrap logo" className={styles.skill}/>
+                        <img src="/logos/Tailwind_CSS_Logo.svg" alt="Tailwind logo" className={styles.skill}/>
+                        <img src="/logos/jquery-logo.svg" alt="jQuery logo" className={styles.skill}/>
+                        <img src="/logos/flask.svg" alt="Flask logo" className={styles.skill}/>
+                        <img src="/logos/mysql-logo.svg" alt="MySQL logo" className={styles.skill}/>
+                        <img src="/logos/mongodb.svg" alt="MongoDB logo" className={styles.skill}/>
+                        <img src="/logos/expressjs-icon.svg" alt="Express.js logo" className={styles.skill}/>
+                        <img src="/logos/React-icon.svg" alt="React logo" className={styles.skill}/>
+                        <img src="/logos/Node.js_logo.svg" alt="Node.js logo" className={styles.skill}/>
+                        <img src="/logos/Amazon_Web_Services_Logo.svg" alt="AWS logo" className={styles.skill}/>
+                        <img src="/logos/postman.svg" alt="Postman logo" className={styles.skill}/>
+                        <img src="/logos/Mathematica_Logo.svg" alt="Mathematica logo" className={styles.skill}/>
                     </div>
                 </section>
                 <section className={styles.about}>

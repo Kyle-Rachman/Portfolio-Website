e302ab32e6cd6e8d150bf40f55045d489e8f4d4a
Kyle-Rachman <kcrachman@gmail.com>
2023-10-13 09:49:25 -0700

    Error and form submissions display now

--- client/src/views/Contact/Contact.tsx
+++ client/src/views/Contact/Contact.tsx
@@ -14,26 +14,54 @@ const Contact = () => {
         happy: "",
         timeframe: "None"
     })
+    const [missing, setMissing] = useState({
+        missingName: false,
+        missingEmail: false,
+        missingHelp: false
+    })
     const formInput: string = "px-[15px] py-[5px] bg-white text-black placeholder:text-black placeholder:font-light rounded-md"
     const navigate = useNavigate();
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        let response = await axios.post("http://localhost:8000/api/submitContactForm", {
-            headers: {
-                "Content-Type": "application/json;charset=utf-8",
-            },
-            body: formDetails,
-        })
-        setSubmittedStatus(true)
-        setFormDetails({
-            name: "",
-            email: "",
-            phone: "",
-            help: "",
-            happy: "",
-            timeframe: "None"
-        })
+        if (formDetails.name && formDetails.email && formDetails.help) {
+            setMissing({
+                missingName: false,
+                missingEmail: false,
+                missingHelp: false
+            })
+            let response = await axios.post("http://localhost:8000/api/submitContactForm", {
+                headers: {
+                    "Content-Type": "application/json;charset=utf-8",
+                },
+                body: formDetails,
+            })
+            setSubmittedStatus(true)
+            setFormDetails({
+                name: "",
+                email: "",
+                phone: "",
+                help: "",
+                happy: "",
+                timeframe: "None"
+            })
+        } else {
+            if (!formDetails.name) {
+                setMissing((prev) => ({...prev, missingName: true}))
+            } else {
+                setMissing((prev) => ({...prev, missingName: false}))
+            }
+            if (!formDetails.email) {
+                setMissing((prev) => ({...prev, missingEmail: true}))
+            } else {
+                setMissing((prev) => ({...prev, missingEmail: false}))
+            }
+            if (!formDetails.help) {
+                setMissing((prev) => ({...prev, missingHelp: true}))
+            } else {
+                setMissing((prev) => ({...prev, missingHelp: false}))
+            }
+        }
     }
 
     return (
@@ -41,18 +69,24 @@ const Contact = () => {
             <div className={styles.contact}>
                 <div className={styles.contactText}>
                     <h2 className='text-5xl font-semibold'>Want to work on solutions <span className='text-accent-purple'>together?</span></h2>
-                    <p className='text-lg text-accent-yellow font-light'>|| Contact</p>
-                    <br />
+                    <p className='text-lg text-accent-yellow font-light mb-4'>|| Contact</p>
                     <p className='text-xl'>Fill in the form, or just send me an email at: <br /> <a className='text-accent-orange transition-color duration-500 hover:drop-shadow-accent-orange' href="mailto:kcrachman@gmail.com" target='_blank' rel="noopener" aria-label='Email'>kcrachman@gmail.com</a></p>
                 </div>
-                {!submittedStatus && <form id={styles.contactForm} onSubmit={handleSubmit}>
+                {!submittedStatus ?
+                <form id={styles.contactForm} onSubmit={handleSubmit}>
                     <div className={styles.formGroup}>
                         <label htmlFor="name" className="font-semibold">Name *</label>
                         <input type="text" className={formInput} id="name" name="name" placeholder="Your name" value={formDetails.name} onChange={(e) => setFormDetails((prev) => ({...prev, name: e.target.value}))}/>
+                        {
+                            missing.missingName && <p className='text-md text-red-600'>Please add your name!</p>
+                        }
                     </div>
                     <div className={styles.formGroup}>
                         <label htmlFor="email" className="font-semibold">Email *</label>
                         <input type="email" className={formInput} id="email" name="email" placeholder="Your email" value={formDetails.email} onChange={(e) => setFormDetails((prev) => ({...prev, email: e.target.value}))}/>
+                        {
+                            missing.missingEmail && <p className='text-md text-red-600'>Please add an email!</p>
+                        }
                     </div>
                     <div className={styles.formGroup}>
                         <label htmlFor="phone" className="font-semibold">Phone</label>
@@ -61,6 +95,9 @@ const Contact = () => {
                     <div className={styles.formGroup}>
                         <label htmlFor="help" className="font-semibold">How can I best help you? *</label>
                         <textarea className={formInput} id="help" rows={2} name="help" placeholder="What are you looking for from me?" value={formDetails.help} onChange={(e) => setFormDetails((prev) => ({...prev, help: e.target.value}))}></textarea>
+                        {
+                            missing.missingHelp && <p className='text-md text-red-600'>Please add some details!</p>
+                        }
                     </div>
                     <div className={styles.formGroup}>
                         <label htmlFor="happy" className="font-semibold">What would make you happy in this project?</label>
@@ -76,7 +113,11 @@ const Contact = () => {
                         </select>
                     </div>
                     <button type="submit" className='border border-white p-2 text-white transition-color duration-500 hover:text-black hover:bg-white font-bold'>Get In Touch</button>
-                </form>}
+                </form> :
+                <div>
+                    <p className='text-4xl'>Thank you!</p>
+                    <p className='text-2xl'>Your submission has been received!</p>
+                </div>}
             </div>
             <div className={styles.moreInfo}>
                 <ToggleOnScroll>

cc8b4a7b03d495f83cfb568be12d091f18286c93
Kyle-Rachman <kcrachman@gmail.com>
2023-10-29 21:58:55 -0700

    Made ProjectCarousel DRY

--- client/src/components/ProjectCarousel.tsx
+++ client/src/components/ProjectCarousel.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'
+import ProjectInfos from '../assets/projectinfo.json'
 import ProjectCard from './ProjectCard/ProjectCard'
 import ToggleOnScroll from './ToggleOnScroll'
 
-// UPDATES: To add a new project, simply update numProjects and add an extra ProjectCard with the appropriate name/alt props
-
 const ProjectCarousel = () => {
     const [activeProject, setActiveProject] = useState(0)
-    const numProjects = 3
+    const numProjects: number = Object.keys(ProjectInfos).length
     const swipe = (left: boolean) => {
         if (left) {
             setActiveProject(prev => ((((prev+1) % numProjects) + numProjects) % numProjects))
@@ -27,21 +26,16 @@ const ProjectCarousel = () => {
                     <div className='flex justify-center items-center gap-[40px] mt-[-50px] md:mt-0'>
                         <button className='mt-20 border border-white text-white transition-color duration-500 hover:text-black hover:bg-white rounded-full h-16 w-16 bg-background-black hidden md:block'
                         onClick={() => setActiveProject(prev => ((((prev-1) % numProjects) + numProjects) % numProjects))}>&lt;</button>
-                        {(activeProject==0) && <ProjectCard
-                            project='bingo'
-                            alt='Bingo Screen'
-                            onSwipe={swipe}
-                        />}
-                        {(activeProject==1) && <ProjectCard
-                            project='test'
-                            alt='Placeholder'
-                            onSwipe={swipe}
-                        />}
-                        {(activeProject==2) && <ProjectCard
-                            project='math'
-                            alt='Placeholder'
-                            onSwipe={swipe}
-                        />}
+                        {
+                        Object.values(ProjectInfos).map((value, idx) => (
+                            (activeProject==idx) && <ProjectCard
+                                key={idx}
+                                project={value.linkname}
+                                alt={value.mainImage[1]}
+                                onSwipe={swipe}
+                            />
+                        ))
+                    }
                         <button className='mt-20 border border-white text-white transition-color duration-500 hover:text-black hover:bg-white rounded-full h-16 w-16 bg-background-black hidden md:block'
                         onClick={() => setActiveProject(prev => ((((prev+1) % numProjects) + numProjects) % numProjects))}>&gt;</button>
                     </div>
